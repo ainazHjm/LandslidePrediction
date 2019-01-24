a362db5aa8e73f5ef5dabc6b425cf1e6e5173a29
anonymous <anonymous>
2019-01-23 17:02:27 -0800

    changes applied to model, changed loss function and added support for ignoring data when the pixel value is -100(no data), stadardized train and validation data

--- data.py
+++ data.py
@@ -6,8 +6,6 @@ import numpy as np
 import random
 
 # pylint: disable=E1101
-bg_size = (221, 149)
-
 def padding(image):
     (row, col) = image.shape
     padded_img = th.zeros(row+2, col+2)
@@ -18,26 +16,25 @@ def divide(data):
     """
     This function divides the dataset into train and validation sets.
     For now, each feature is divided into 5 parts and the validation
-    is one of these parts which is selected at random. The rest is the
+    is one of these parts(index=1). The rest is the
     training set.
-    :data: the dataset in shape of a 3D array: 5x6630x9387.
+    :data: the dataset in shape of a 3D array: 5xhxw.
     """
-    div = data.shape[2]/5
-    val_idx = np.array(int(random.random()*5))
-    np.save(val_idx, "../image_data/data/val_idx.npy")
+    div = data.shape[2]//5
+    val_idx = np.array(1)
+    np.save("../image_data/data/val_idx.npy", val_idx)
     
-    val_data = data[:, :, val_idx[0]*div:(val_idx[0]+1)*div]
-    train_data = th.cat((data[:, :, 0:val_idx[0]*div], data[:, :, (val_idx[0]+1)*div:]), 1)
+    val_data = data[:, :, val_idx*div:(val_idx+1)*div]
+    train_data = th.cat((data[:, :, 0:val_idx*div], data[:, :, (val_idx+1)*div:]), 2)
     th.save(val_data, "../image_data/data/val_data.pt")
     th.save(train_data, "../image_data/data/train_data.pt")
-    return val_idx[0], val_data, train_data
+    return val_idx, val_data, train_data
 
 def zero_one(image):
-    # image = th.from_numpy(image)
-    n_img = image
-    n_img[image == 255] = 1
-    n_img[image != 255] = 0
-    return n_img
+    image = image.long()
+    image[image == 255] = 1
+    image[image == 100] = 0
+    return image
 
 def process(dir_path='../image_data/n_image_data/'):
     """
@@ -55,14 +52,16 @@ def process(dir_path='../image_data/n_image_data/'):
 
     for i, e in enumerate(img_names):
         img = Image.open(dir_path+e)
-        img = transforms.ToTensor(img)
-        # p_img = padding(img)
-        data[i, :, :] = zero_one(p_img) if i == 4 else p_img
-    # if divide:
-    #     val_idx, val_data, train_data = divide(data)
-    # else:
-    #     val_idx = np.load("../image_data/data/val_idx.npy")
-    #     val_data = th.load("../image_data/data/val_data.pt")
-    #     train_data = th.load("../image_data/data/train_data.pt")
-    # return val_idx, val_data, train_data
+        img = transforms.ToTensor()(img)
+        data[i, :, :] = zero_one(img) if i == 4 else img
     return data
+
+def normalize(train_data, val_data):
+    (c, h, w) = train_data.shape
+    td = train_data.cuda().view(-1)
+    mean = th.mean(td)
+    std = th.std(td)
+
+    train_data = ((td-mean)/std).view(c, h, w)
+    vd = (val_data-mean)/std
+    return train_data, vd
