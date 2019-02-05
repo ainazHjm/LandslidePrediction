3542c69d8acf03329c6487bf0ed83817d6df07b0
Ainaz Hajimoradlou <ainaz@cs.ubc.ca>
2019-02-05 14:20:08 -0800

    added create_geotif function

--- utils/plot.py
+++ utils/plot.py
@@ -1,3 +1,5 @@
+import torch as th
+import numpy as np
 from matplotlib import pyplot
 from torch.nn import Sigmoid
 from torch import save
@@ -18,4 +20,19 @@ def save_results(args, model, val_data):
         # Image.fromarray(im.numpy()).save("../output/val_res"+name+".tif")
     # im = predictions.view(h, w).detach()
     # pyplot.imshow(im.numpy())
-    return name
+
+def create_geotif(
+    name,
+    val_idx = '../image_data/data/val_idx.npy',
+    predictions = '../output/'+name+'.pt',
+    train_data = '../image_data/data/train_data.pt',
+    )
+    vi = np.load(val_idx)
+    prds = th.load(predictions)
+    td = th.load(train_data)
+    (h, w1) = prdss.shape
+    (_, w2) = td.shape
+    res = th.zeros(h, w1+w2)
+    div = (w1+w2)//5
+    res = th.cat((td[:, 0:vi*div], prds, td[:, vi*div:]), 0)
+    save_image(res, "../output/"+name+".pt")
