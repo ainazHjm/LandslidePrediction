2e41d07e4890c1497b63b09a9e0d06197fb82063
anonymous <anonymous>
2019-02-15 11:45:10 -0800

    updated version

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+# pylint: disable=E0611
 from data import process, normalize
 from train import train, find_accuracy, cross_validate
 import argparse
 import torch as th
 import numpy as np
-# pylint: disable=E0611
 from utils.args import str2bool
 from utils.plot import save_results
 
@@ -17,35 +17,28 @@ def get_args():
     parser.add_argument("--batch_size", type=int, default=4)
     parser.add_argument("--decay", type=float, default=1e-5)
     parser.add_argument("--load_model_path", type=str, default='')
+    # parser.add_argument("--retrain_model", type=str2bool, default=False)
     # parser.add_argument("--save_features", type=str2bool, default=False)
-    parser.add_argument("--save_result", type=str2bool, default=False)
+    parser.add_argument("--validate", type=str2bool, default=False)
     return parser.parse_args()
 
 def main():
     args = get_args()
-    # data = process()
-
     if args.cross_validation:
         data = process()
         train_data, val_data, val_idx = cross_validate(args, data)
     else:
         # the data that is loaded is standardized with mean 0 and std 1
-        val_data = th.load("../image_data/data/val_data.pt")
-        val_idx = np.load("../image_data/data/val_idx.npy")
-        train_data = th.load("../image_data/data/train_data.pt")
-
+        val_data = th.load("../image_data/data/CNN/val_data.pt")
+        val_idx = np.load("../image_data/data/CNN/val_idx.npy")
+        train_data = th.load("../image_data/data/CNN/train_data.pt")
     # td, vd = normalize(train_data, val_data) # the data is normalized before
-    
-    if args.load_model_path:
+    if args.validate:
         print("loading a trained model...")
-        model = th.load(args.load_model_path).cuda()
-        # acc = find_accuracy(model, vd)
-        # print(">> accuracy on the validation set: %f" % acc)
-        visualise(args, model, vd)
+        model = th.load(args.load_model_path)
+        save_results(model, val_data)
     else:
-        print("model is training ...")
-        loss = train(args, td, vd)
-        print("model has been trained.")
+        loss = train(args, train_data, val_data)
 
 if __name__ == "__main__":
     main()
