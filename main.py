fixed some bugs for rotation

main.py
@@ -36,21 +36,21 @@ def main():
     #     # the data that is loaded is standardized with mean 0 and std 1
     #     val_data = th.load("../image_data/data/Veneto/nan/val_data.pt")
     #     val_idx = np.load("../image_data/data/Veneto/val_idx.npy")
-    td = np.load(args.data_path+'tdIdx.npy')
-    vd = np.load(args.data_path+'vdIdx.npy')
-    print("data index is loaded ...")
+    # td = np.load(args.data_path+'tdIdx.npy')
+    # vd = np.load(args.data_path+'vdIdx.npy')
+    # print("data index is loaded ...")
 
     if args.validate:
         print("loading a trained model...")
         print("validating the model on validation data ...")
         model = th.load(args.load_model)
-        save_results(args, model, vd)
+        save_results(args, model, 'validation')
         print("validating the model on training data ...")
-        save_results(args, model, td)
+        save_results(args, model, 'train')
         print("model is validated and the results are saved.")      
     else:
         print("starting to train ...")
-        train.train(args, td, vd)
+        train.train(args)
 
 if __name__ == "__main__":
     main()

train.py
@@ -4,58 +4,83 @@ import torch as th
 import torch.optim as to
 import torch.nn as nn
 import os
-import imutils
+import scipy.ndimage
+# import imutils
 from time import ctime
 from tensorboardX import SummaryWriter
 from torchvision.utils import save_image
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 # pylint: disable=E1101,E0401,E1123
 
-def load_data(args, fname, angle, feature_num=21):
+def find_max_shape(path):
+    img = np.load(path)
+    img = np.asarray(img, dtype='float32')
+    max_h, max_w = 0, 0
+    for angle in np.arange(0, 360, 5):
+        rotated = scipy.ndimage.rotate(img, angle)
+        (h, w) = rotated.shape
+        if h > max_h:
+            max_h = h
+        if w > max_w:
+            max_w = w
+    return (max_h, max_w)
+
+def load_data(args, fname, angle, data_max_shape, gt_max_shape, feature_num=21):
     dp = args.data_path
-    data_dir = 'data_'+str(args.pad*2)
+    data_dir = 'data_'+str(args.pad*2)+'/'
     label_dir = 'gt_200/'
     data = []
     label = []
+    # (h, w) = find_max_shape(dp+data_dir+'0_'+fname[0])
+    (h, w) = data_max_shape
+    (gt_h, gt_w) = gt_max_shape
     for name in fname:
         features = []
         for i in range(feature_num):
             img = np.load(dp+data_dir+str(i)+'_'+name)
-            rotated = imutils.rotate_bound(img, angle)
-            padding = img.shape[0] + img.shape[0]//2 - rotated.shape[0]
-            if padding < 0:
+            img = np.asarray(img, dtype='float32')
+            rotated = scipy.ndimage.rotate(img, angle)
+            hdif = h - rotated.shape[0]
+            wdif = w - rotated.shape[1]
+            if hdif< 0 or wdif < 0:
                 raise ValueError
-            features.append(np.pad(rotated, padding, 'constant')) # 2d shape
+            features.append(np.pad(rotated, ((hdif//2, hdif-hdif//2), (wdif//2, wdif-wdif//2)), 'constant')) # 2d shape
         features = np.asarray(features)
         data.append(features)
         gt = np.load(dp+label_dir+name)
-        rotated_gt = imutils.rotate_bound(gt, angle)
-        padding = gt.shape[0] + gt.shape[0]//2 - rotated_gt.shape[0]
-        if padding < 0:
+        rotated = scipy.ndimage.rotate(gt, angle)
+        gt_hdif = gt_h - rotated.shape[0]
+        gt_wdif = gt_w - rotated.shape[1]
+        if gt_hdif < 0 or gt_wdif < 0:
             raise ValueError
-        gt = np.pad(rotated_gt, padding, 'constant')
+        gt = np.pad(rotated, ((gt_hdif//2, gt_hdif-gt_hdif//2), (gt_wdif//2, gt_wdif-gt_wdif//2)), 'constant')
         label.append(gt.reshape(1, gt.shape[0], gt.shape[1]))
     return np.asarray(data), np.asarray(label) #4d shape
 
-def validate(args, model, valIdx):
+def validate(args, model, data_max_shape, gt_max_shape):
     with th.no_grad():
+        valIdx = np.load(args.data_path+'vdIdx.npy')
+        np.random.shuffle(valIdx)
         criterion = nn.BCEWithLogitsLoss(pos_weight=th.Tensor([1000]).cuda())
         # criterion = nn.BCEWithLogitsLoss()
+        # hdif = max_shape[0] - 200
+        # wdif = max_shape[1] - 200
         running_loss = 0
         bs = args.batch_size
         #bs = 1
         num_iters = valIdx.shape[0]//bs
         for i in range(num_iters):
             # in_d, gt = load_data(args, valIdx[i*bs:(i+1)*bs]) if i < num_iters else load_data(args, valIdx[i*bs:])
-            in_d, gt = load_data(args, valIdx[i*bs:(i+1)*bs], 0)
+            in_d, gt = load_data(args, valIdx[i*bs:(i+1)*bs], 0, data_max_shape, gt_max_shape)
             in_d, gt = th.tensor(in_d).cuda(), th.tensor(gt).float().cuda()
             # print(in_d.shape, gt.shape, valIdx[i])
             prds = model.forward(in_d)
-            loss = criterion(prds[:, :, args.pad:args.pad+200, args.pad:args.pad+200].view(-1, 1, 200, 200), gt)
+            # loss = criterion(prds[:, :, hdif//2:hdif//2+200, wdif//2:wdif//2+200].view(-1, 1, 200, 200), gt)
+            loss = criterion(prds[:, :, args.pad:-args.pad, args.pad:-args.pad].view(-1, 1, data_max_shape[0]-args.pad*2, data_max_shape[1]-args.pad*2), gt)
             running_loss += loss.item()
         return running_loss/num_iters
 
-def train(args, train_data, val_data):
+def train(args):
     '''
     Trains on a batch of patches of size (22, 200, 200).
     TODO: 
@@ -71,12 +96,16 @@ def train(args, train_data, val_data):
     dir_name = args.save_model_to + exe_time if args.c else args.save_model_to
     if not os.path.exists(dir_name):
         os.mkdir(dir_name)
+    train_data = np.load(args.data_path+'tdIdx.npy')
+    data_max_shape = find_max_shape(args.data_path+'data_'+str(2*args.pad)+'/0_2_3.npy')
+    gt_max_shape = (data_max_shape[0]-2*args.pad, data_max_shape[1]-2*args.pad)
+    # hdif = max_shape[0] - 200
+    # wdif = max_shape[1] - 200
     # train_data, train_label = make_patches(train_data_path)
-    train_model = model.FCN().cuda() if args.model == "FCN" else model.FCNwPool((22-1, 200+args.pad*2, 200+args.pad*2), args.pix_res).cuda()
+    train_model = model.FCN().cuda() if args.model == "FCN" else model.FCNwPool((22-1, data_max_shape[0], data_max_shape[1]), args.pix_res).cuda()
     if args.load_model:
         train_model.load_state_dict(th.load(args.load_model).state_dict())
     print("model is initialized ...")
-
     optimizer = to.Adam(train_model.parameters(), lr = args.lr, weight_decay = args.decay)
     # scheduler = ReduceLROnPlateau(optimizer, mode='min', patience=2)
     criterion = nn.BCEWithLogitsLoss(pos_weight=th.Tensor([1000]).cuda())
@@ -86,40 +115,49 @@ def train(args, train_data, val_data):
     num_iters = train_data.shape[0]//bs
     print(num_iters)
     running_loss = 0
-    
+    loss_100 = 0
+    # loss_20 = 0
+    # max_shape = find_max_shape(args.data_path+'data_'+str(2*args.pad)+'/0_2_3.npy')
     for i in range(args.n_epochs):
         # running_loss = 0
         # shuffling the train and validation indices
         np.random.shuffle(train_data)
-        np.random.shuffle(val_data)
+        # np.random.shuffle(val_data)
 
         # if (i+1) % args.s == 0:
         #    th.save(train_model, dir_name+'/'+str(i)+'_'+exe_time+'.pt')
 
         for j in range(num_iters):
-            for angle in np.arange(0, 360, 5):
+            for k, angle in enumerate(np.arange(0, 360, 5)):
                 optimizer.zero_grad()
-                in_d, gt = load_data(args, train_data[j*bs:(j+1)*bs], angle)
+                in_d, gt = load_data(args, train_data[j*bs:(j+1)*bs], angle, data_max_shape, gt_max_shape)
+                # print('input data shape: (%d, %d, %d, %d)' % in_d.shape)
+                # print('ground truth shape: (%d, %d, %d, %d)' % gt.shape)
                 in_d, gt = th.tensor(in_d).cuda(), th.tensor(gt).float().cuda()
                 # print(in_d.shape, gt.shape)
                 prds = train_model.forward(in_d)
                 # print(prds.shape)
-                loss = criterion(prds[:, :, args.pad:args.pad+200, args.pad:args.pad+200].view(-1, 1, 200, 200), gt)
-                if (i*num_iters+j+1) % 20 == 0:
-                    writer.add_scalar("loss/train@20", loss.item(), i*num_iters+j)
-                    print("%d,%d >> loss: %f" % (i, j, loss.item()))
+                # loss = criterion(prds[:, :, hdif//2:hdif//2+200, wdif//2:wdif//2+200].view(-1, 1, 200, 200), gt)
+                loss = criterion(prds[:, :, args.pad:-args.pad, args.pad:-args.pad].view(-1, 1, data_max_shape[0]-args.pad*2, data_max_shape[1]-args.pad*2), gt)
+                writer.add_scalar("loss/train_iter", loss.item(), i*num_iters+j)
+                print("%d,%d >> loss: %f" % (i, j, loss.item()))
                 running_loss += loss.item()
+                loss_100 += loss.item()
+                # loss_20 += loss.item
+                if (i*num_iters*360/5+j*360/5+k+1) % 100 == 0:
+                    writer.add_scalar("loss/train_100", loss_100/100, i*num_iters*360/5+j*360/5+k+1)
+                    loss_100 = 0
                 loss.backward()
                 optimizer.step()
 
-                if (i*num_iters+j+1) % 1000 == 0:
+                if (i*num_iters*360/5+j*360/5+k+1) % 1000 == 0:
                     del in_d, gt, prds
-                    v_loss = validate(args, train_model, val_data)
+                    v_loss = validate(args, train_model, max_shape)
                     # scheduler.step(v_loss)
                     print("--- validation loss: %f" % v_loss)
-                    writer.add_scalars("loss/grouped", {'validation': v_loss, 'train': running_loss/1000}, i*num_iters+j)
-                    writer.add_scalar("loss/validation", v_loss, i*num_iters+j)
-                    writer.add_scalar("loss/train", running_loss/1000, i*num_iters+j)
+                    writer.add_scalars("loss/grouped", {'validation': v_loss, 'train': running_loss/1000}, i*num_iters*360/5+j*360/5+k+1)
+                    writer.add_scalar("loss/validation", v_loss, i*num_iters*360/5+j*360/5+k+1)
+                    writer.add_scalar("loss/train", running_loss/1000, i*num_iters*360/5+j*360/5+k+1)
                     running_loss = 0
 
                     for name, param in train_model.named_parameters():

utils/plot.py
@@ -13,25 +13,27 @@ from torchvision.utils import save_image
 # from train import load_data
 
 def data_loader(args, fname, feature_num=21):
+    max_shape = (464, 464)
     dp = args.data_path
-    data_dir = 'data_600/'
+    data_dir = 'data_'+str(args.pad*2)+'/'
     # label_dir = 'gt_200/'
     data = []
     names = []
     for name in fname:
         features = []
         for i in range(feature_num):
-           features.append(np.load(dp+data_dir+str(i)+'_'+name))
+           features.append(np.pad(np.load(dp+data_dir+str(i)+'_'+name), 68, 'constant'))
         features = np.asarray(features)
         data.append(features)
         names.append(name)
     return np.asarray(data), np.asarray(names) #4d shape
 
-def save_results(args, model, data_idx, pad=64):
+def save_results(args, model, idx):
     th.cuda.empty_cache()
     dir_name = args.save_res_to + args.load_model.split('/')[-1].split('.')[0]
     if not os.path.exists(dir_name):
         os.mkdir(dir_name)
+    data_idx = np.load(args.data_path+'tdIdx.npy') if idx=='train' else np.load(args.data_path+'vdIdx.npy')
     sig = Sigmoid()
     # bs = args.batch_size
     num_iters = (data_idx.shape[0])
@@ -43,7 +45,8 @@ def save_results(args, model, data_idx, pad=64):
         prds[ignore.unsqueeze(1)] = 0
         for j in range(prds.shape[0]):
             # save_image(prds[j, 0, :, :], dir_name+'/'+names[j].split('.')[0]+'.tif', range=(0,1))
-            np.save(dir_name+'/'+names[j], prds[j, 0, pad:pad+200, pad:pad+200].cpu().data.numpy())
+            np.save(dir_name+'/'+names[j], prds[j, 0, 132:132+200, 132:132+200].cpu().data.numpy())
+            # np.save(dir_name+'/'+names[j], prds[j, 0, args.pad:-args.pad, args.pad:-args.pad].cpu().data.numpy())
 
 def unite_imgs(data_path, orig_shape, ws):
     img_names = os.listdir(data_path)