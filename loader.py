a89245bfca962711e2456c0cf06a0ed50dde4484
anonymous <anonymous>
2019-05-24 16:33:31 -0700

    working version of data-loader with oversampling method and improved loading speed

--- loader.py
+++ loader.py
@@ -4,16 +4,18 @@ import numpy as np
 import torch as th
 
 class LandslideTrainDataset(Dataset):
-    def __init__(self, path, region, stride, ws, pts, pad=64):
+    def __init__(self, path, region, stride, ws, pts, oversample_path, pad=64, feature_num=94):
         super(LandslideTrainDataset, self).__init__()
         self.path = path
         self.ws = ws
         self.stride = stride
         self.region = region
         self.pad = pad
+        self.feature_num = feature_num
         self.pts = pts #nx4 matrix consisting of r1, c1, r2, c2 points, r1<r2 & c1<c2
         self.data_len = self.len()
         self.pts_len = self.len_oversample() #nx1 matrix containing length of the data corresponding to pts
+        self.oversample_path = oversample_path
 
     def identify_idx(self, index):
         cum_sum = 0
@@ -25,7 +27,6 @@ class LandslideTrainDataset(Dataset):
 
     def len_oversample(self):
         stride = self.stride//10
-        leng = 0
         pts_len = np.zeros((self.pts.shape[0], 1))
         for i in range(self.pts.shape[0]):
             h = self.pts[i,2] - self.pts[i,0]
@@ -69,29 +70,19 @@ class LandslideTrainDataset(Dataset):
         return sample
 
     def __getitem__(self, index):
-        with h5py.File(self.path, 'r') as f:
-            dataset = f[self.region]['train']['data']
-            gt = f[self.region]['train']['gt']
-            if index < self.data_len:
-                sample = self.get_item(index, dataset, gt, self.stride)
-            else:
+        if index < self.data_len:
+            with h5py.File(self.path, 'r') as f:
+                dataset = f[self.region]['train']['data']
+                gt = f[self.region]['train']['gt']
+                return self.get_item(index, dataset, gt, self.stride)
+        else:
+            with h5py.File(self.oversample_path, 'r') as g:
                 pts_idx = self.identify_idx(index)
+                # r1, c1, r2, c2 = self.pts[pts_idx, :]
                 n_index = index-self.data_len if pts_idx==0 else index-(self.data_len+int(np.sum(self.pts_len[:pts_idx])))
-                sample = self.get_item(
-                    n_index,
-                    dataset[
-                        :,
-                        self.pts[pts_idx, 0]:self.pts[pts_idx, 2],
-                        self.pts[pts_idx, 1]:self.pts[pts_idx, 3]
-                    ],
-                    gt[
-                        :,
-                        self.pts[pts_idx, 0]:self.pts[pts_idx, 2],
-                        self.pts[pts_idx, 1]:self.pts[pts_idx, 3]
-                    ],
-                    self.stride//10
-                )  
-            return sample
+                dataset = g[str(pts_idx)]['data']
+                gt = g[str(pts_idx)]['gt']
+                return self.get_item(n_index, dataset, gt, self.stride//10)
 
 class LandslideDataset(Dataset):
     '''
@@ -139,3 +130,42 @@ class LandslideDataset(Dataset):
             }
             return sample
         
+def initilize_data_oversample(args, fw):
+    with h5py.File(args.data_path, 'r') as f:
+        dataset = f[args.region]['train']['data']
+        gt = f[args.region]['train']['gt']
+        for idx in range(args.oversample_pts.shape[0]):
+            r1, c1, r2, c2 = args.oversample_pts[idx, :]
+            h, w = r2-r1, c2-c1
+            fw[str(idx)]['data'][:, 0:args.pad, :] = 0
+            fw[str(idx)]['data'][:, h+args.pad:, :] = 0
+            fw[str(idx)]['data'][:, :, 0:args.pad] = 0
+            fw[str(idx)]['data'][:, :, w+args.pad:] = 0
+            fw[str(idx)]['data'][:, args.pad:args.pad+h, args.pad:args.pad+w] = dataset[:, r1:r2, c1:c2]
+            fw[str(idx)]['gt'][:][:, :, :] = gt[:, r1:r2, c1:c2]
+    return fw
+
+def create_oversample_data(args):
+    path = '/'.join(args.data_path.split('/')[:-1])+'/'+args.region+'_oversample.h5'
+    # path = '/home/ainaz/Projects/Landslides/image_data/'+args.region+'_oversample.h5'
+    if not args.oversample:
+        return path
+    f = h5py.File(path, 'a')
+    for i in range(args.oversample_pts.shape[0]):
+        r1, c1, r2, c2 = args.oversample_pts[i, :]
+        f.create_dataset(
+            str(i)+'/data',
+            (args.feature_num, r2-r1+2*args.pad, c2-c1+2*args.pad),
+            dtype='f',
+            compression='lzf'
+        )
+        f.create_dataset(
+            str(i)+'/gt',
+            (1, r2-r1, c2-c1),
+            dtype='f',
+            compression='lzf'
+        )
+    f = initilize_data_oversample(args, f)
+    f.close()
+    return path
+    
