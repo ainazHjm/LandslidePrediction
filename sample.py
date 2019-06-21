a9b481a9d2a69e7ab6a491c6c28c28ae039872ec
anonymous <anonymous>
2019-06-20 17:55:51 -0700

    working version of the sampled network

--- sample.py
+++ sample.py
@@ -9,17 +9,16 @@ def get_args():
     parser.add_argument('--save_to', type=str, default='/home/ainaz/Projects/Landslides/image_data/')
     return parser.parse_args()
 
-def sample(args):
-    gt = np.array(Image.open(args.gt_path))
-    ones = (gt==1).nonzero() # gives two arrays: array 1 consists of rows and array 2 consists of cols
-    zeros = (gt==0).nonzero()
+def sample_fn(args, data, flag='train'):
+    ones = (data==1).nonzero() # gives two arrays: array 1 consists of rows and array 2 consists of cols
+    zeros = (data==0).nonzero()
     num_samples = ones[0].shape[0]
     zero_samples_idx = np.random.choice(np.arange(0, zeros[0].shape[0]), size=num_samples, replace=False)
     one_samples = np.array(
         list(
             map(
                 lambda idx: (ones[0][idx], ones[1][idx]),
-                np.arange(0, len(ones[0]))
+                np.arange(0, num_samples)
             )
         )
     )
@@ -31,16 +30,20 @@ def sample(args):
             )
         )
     )
-    # import ipdb; ipdb.set_trace()
-    np.save(args.save_to+'ones.npy', one_samples)
-    np.save(args.save_to+'zeros.npy', zero_samples)
-    data = np.concatenate((one_samples, zero_samples))
-    np.random.shuffle(data)
-    idx = (8*len(data))//10
-    train_data = data[0:idx]
-    test_data = data[idx:]
-    np.save(args.save_to+'train_data.npy', train_data)
-    np.save(args.save_to+'test_data.npy', test_data)
+    res = np.concatenate((one_samples, zero_samples))
+    np.random.shuffle(res)
+    np.save(args.save_to+flag+'_data.npy', res)
+    
+def sample(args):
+    gt = np.array(Image.open(args.gt_path))
+    (h, _) = gt.shape
+
+    train_gt = np.concatenate((gt[0:h//5, :], gt[2*(h//5):, :]))
+    sample_fn(args, train_gt, 'train')
+
+    test_gt = gt[h//5:2*(h//5), :]
+    sample_fn(args, test_gt, 'test')
+    
 
 def main():
     args = get_args()
