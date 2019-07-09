dc93a8732c01df94de39cd51d0de8394da05a707
ainaz <ainaz.hajimoradlou@gmail.com>
2019-07-09 10:42:05 -0700

    updated validate function

--- validate.py
+++ validate.py
@@ -1,27 +1,35 @@
 import torch as th
 import torch.nn as nn
+import numpy as np
 from torchvision.utils import save_image
+from torch.utils.data import DataLoader
 from loader import RotDataset
 from time import ctime
 from sacred import Experiment
 
-def validate(params, test_dataset, _log):
-    sig = nn.Sigmoid()
-    shape = params['shape']
-    r, c = shape[0]//params['ws'], shape[1]//params['ws']
-    res = th.zeros(shape)
-    pad = params['pad']
-    model = th.load(params['load_model'])
-    _log.info('[{}] model is successfully loaded.'.format(ctiem()))
-    for idx_ in range(len(test_dataset)):
-        data, gt = test_dataset[idx_]['data'].cuda().unqueeze(0), test_dataset[idx_]['gt'].cuda()
-        ignore = gt < 0
-        prds = model.forward(data)[:, :, pad:-pad, pad:-pad]
-        prds[ignore] = 0
-        row, col = idx_//c, idx_-(idx_//c)*c
-        res[row:row+params['ws'], col:col+params['ws']] = prds.squeeze(0)
-        _log.info('[{}]: writing [{}/{}]'.format(ctime(), idx_, len(test_dataset)))
-    save_image(res, params['save_to']+'predictions.tif')
+def validate(params, test_loader, _log):
+    with th.no_grad():
+        sig = nn.Sigmoid()
+        shape = params['shape']
+        res = th.zeros(shape)
+        pad = params['pad']
+        model = th.load(params['load_model'])
+        _log.info('[{}] model is successfully loaded.'.format(ctime()))
+        test_iter = iter(test_loader)
+        for iter_ in range(len(test_iter)):
+            sample = test_iter.next()
+            data, gt = sample['data'].cuda(), sample['gt'].cuda()
+            ignore = gt < 0
+            prds = sig(model.forward(data))[:, :, pad:-pad, pad:-pad]
+            prds[ignore] = 0
+            del data, gt, ignore
+            for idx in range(prds.shape[0]):
+                row, col = sample['index'][0][idx], sample['index'][1][idx]
+                res[row*params['ws']:(row+1)*params['ws'], col*params['ws']:(col+1)*params['ws']] = prds[idx, 0, :, :]
+            _log.info('[{}]: writing [{}/{}]'.format(ctime(), iter_, len(test_iter)))
+        _log.info('all images are written!')
+        save_image(res, '{}{}_predictions.tif'.format(params['save_to'], params['flag']))
+        np.save('{}{}_predictions.npy'.format(params['save_to'], params['flag']), res.data.numpy())
 
 ex = Experiment('validate_rotation')
 
@@ -30,15 +38,19 @@ def ex_cfg():
     params = {
         'data_path': '/dev/shm/rotated_dataset.h5',
         'load_model': '',
-        'save_to': 'runs/result/'
+        'save_to': 'runs/result/',
         'region': 'Veneto',
-        'ws': 200,
+        'ws': 400,
         'pad': 64,
-        'shape': (4201, 19250)
+        'shape': (4201, 19250),
+        'flag': 'test',
+        'bs': 5,
+        'n_workers': 4
     }
 
 @ex.automain
-def main(params):
+def main(params, _log):
     vd = RotDataset(params['data_path'], params['region'], 'test')
+    vd_loader = DataLoader(vd, batch_size=params['bs'], num_workers=params['n_workers'])
     _log.info('[{}] prepared the dataset for validation.'.format(ctime()))
-    validate(params, vd, _log)
+    validate(params, vd_loader, _log)
