fixed the bug in adjust rotation: rotating each channel of the image separately

adjust_rotation.py
@@ -15,7 +15,7 @@ def ex_cfg():
         'ws': 400,
         'pad': 64,
         'num_feature': 94,
-        'save_to': '/home/ainaz/Projects/Landslides/image_data/rotated_landslide.h5'
+        'save_to': '/home/ainaz/Projects/Landslides/image_data/rotated_landslide_new.h5'
     }
 
 def rad2deg(theta):
@@ -23,7 +23,7 @@ def rad2deg(theta):
 
 def find_angle(pc, pr):
     theta = np.arctan2(abs(pr[0]-pc[0]), abs(pr[1]-pc[1]))
-    return rad2deg(theta)
+    return 90-int(rad2deg(theta)) # only return the integer values
 
 def init_dataset(args, num_samples):
     f = h5py.File(args['save_to'], 'w')
@@ -50,18 +50,20 @@ def init_dataset(args, num_samples):
         )
     return f
 
-def write_dataset_iter(args, f, sample, angle, idx, data_flag='train'):
-    data = snd.rotate(sample['data'], angle, reshape=False) # TODO: check shape & type
-    gt = snd.rotate(sample['gt'], angle, reshape=False)
-    # import ipdb; ipdb.set_trace()
-    f[args['region']][data_flag]['data'][idx, :, :, :] = data
-    f[args['region']][data_flag]['gt'][idx, :, :, :] = gt
+def write_dataset_iter(args, f, sample, angle, idx, data_flag):
+    gt = sample['gt'][0, :, :]
+    rot_gt = snd.rotate(gt, angle, reshape=False)
+    f[args['region']][data_flag]['gt'][idx, 0, :, :] = rot_gt
     f[args['region']][data_flag]['index'][idx, :] = sample['index']
     f[args['region']][data_flag]['angle'][idx, :] = angle
+    for channel in range(sample['data'].shape[0]): # rotate for each channel
+        data = sample['data'][channel, :, :]
+        rot_data = snd.rotate(data, angle, reshape=False)
+        f[args['region']][data_flag]['data'][idx, channel, :, :] = rot_data
     return f
 
 @ex.capture
-def adjust_rot(args, f, dataset, data_flag, _log):
+def adjust_rot(args, f, dataset, data_flag, _log, _run):
     for idx in range(len(dataset)):
         sample = dataset[idx]
         (h, w) = sample['data'][0, :, :].shape
@@ -70,6 +72,7 @@ def adjust_rot(args, f, dataset, data_flag, _log):
         headpt = np.array([h//2, w//2])
         angle = find_angle(headpt, tailpt)
         _log.info('[{}][{}/{}] ---- angle: {} ---- '.format(ctime(), idx, len(dataset), angle))
+        _run.log_scalar('rotation_{}.angle'.format(data_flag), angle, idx)
         f = write_dataset_iter(args, f, sample, angle, idx, data_flag)
     _log.info('created the rotated dataset for {}'.format(data_flag))
     return f

train.py
@@ -7,6 +7,7 @@ from time import ctime
 from tensorboardX import SummaryWriter
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from utils.plot import save_config
+# from adjust_rotation import rad2deg, find_angle
 # pylint: disable=E1101,E0401,E1123
 
 def create_dir(dir_name):
@@ -21,7 +22,7 @@ def create_dir(dir_name):
 def validate(model, test_loader, train_param, data_param):
     with th.no_grad():
         criterion = nn.BCEWithLogitsLoss(pos_weight=th.Tensor([train_param['pos_weight']]).cuda())
-        running_loss = 0
+        running_loss, ignore = 0, 0
         test_iter = iter(test_loader)
         for _ in range(len(test_iter)):
             batch_sample = test_iter.next()
@@ -29,10 +30,11 @@ def validate(model, test_loader, train_param, data_param):
             prds = model.forward(data)[:, :, data_param['pad']:-data_param['pad'], data_param['pad']:-data_param['pad']]
             indices = gt >= 0
             if prds[indices].nelement() == 0:
+                ignore += 1
                 continue
             loss = criterion(prds[indices], gt[indices])
             running_loss += loss.item()
-        return running_loss/len(test_iter)
+        return running_loss/(len(test_iter)-ignore)
 
 def train(train_loader, test_loader, train_param, data_param, loc_param, _log):
     writer = SummaryWriter()
@@ -54,7 +56,7 @@ def train(train_loader, test_loader, train_param, data_param, loc_param, _log):
     
     loss_ = 0
     for epoch in range(train_param['n_epochs']):
-        running_loss = 0
+        running_loss, ignore = 0, 0
         train_iter = iter(train_loader)
         for iter_ in range(len(train_iter)):
             optimizer.zero_grad()
@@ -64,27 +66,27 @@ def train(train_loader, test_loader, train_param, data_param, loc_param, _log):
             prds = train_model.forward(data)[:, :, data_param['pad']:-data_param['pad'], data_param['pad']:-data_param['pad']]
             indices = gt >= 0
             if prds[indices].nelement() == 0:
+                ignore += 1
                 continue
             loss = criterion(prds[indices], gt[indices]) # check the loss value
-            # import ipdb; ipdb.set_trace()
             running_loss += loss.item()
             loss_ += loss.item()
             
             loss.backward()
             optimizer.step()
 
-            writer.add_scalar("loss/train_iter", loss.item(), epoch*len(train_iter)+iter_+1)
+            writer.add_scalar("loss/train_iter", loss.item(), epoch*len(train_iter)+iter_+1-ignore)
             writer.add_scalars(
                 "probRange",
                 {'min': th.min(sig(prds)), 'max': th.max(sig(prds))},
-                epoch*len(train_iter)+iter_+1
+                epoch*len(train_iter)+iter_+1-ignore
             )
-            if (epoch*len(train_iter)+iter_+1) % 20 == 0:
-                writer.add_scalar("loss/train_100", loss_/20, epoch*len(train_iter)+iter_+1)
+            if (epoch*len(train_iter)+iter_+1-ignore) % 20 == 0:
+                writer.add_scalar("loss/train_20", loss_/20, epoch*len(train_iter)+iter_+1-ignore)
                 _log.info(
                     '[{}] loss at [{}/{}]: {}'.format(
                         ctime(),
-                        epoch*len(train_iter)+iter_+1,
+                        epoch*len(train_iter)+iter_+1-ignore,
                         train_param['n_epochs']*len(train_iter),
                         loss_/20
                     )
@@ -97,13 +99,13 @@ def train(train_loader, test_loader, train_param, data_param, loc_param, _log):
         _log.info('[{}] validation loss at [{}/{}]: {}'.format(ctime(), epoch+1, train_param['n_epochs'], v_loss))
         writer.add_scalars(
             "loss/grouped",
-            {'test': v_loss, 'train': running_loss/len(train_iter)},
+            {'test': v_loss, 'train': running_loss/(len(train_iter)-ignore)},
             epoch
         )
         if (epoch+1) % loc_param['save'] == 0:
-            th.save(train_model, '{}model_at{}.pt'.format(model_dir, epoch))
+            th.save(train_model.state_dict(), '{}model_at{}.pt'.format(model_dir, epoch))
 
     writer.export_scalars_to_json(writer.file_writer.get_logdir()+'/loss.json')
-    th.save(train_model, '{}trained_model.pt'.format(model_dir))
+    th.save(train_model.state_dict(), '{}trained_model.pt'.format(model_dir))
     save_config(writer.file_writer.get_logdir()+'/config.txt', train_param, data_param)
     _log.info('[{}]: model has been trained and config file has been saved.'.format(ctime()))

validate.py
@@ -1,11 +1,13 @@
 import torch as th
 import torch.nn as nn
 import numpy as np
+import matplotlib.pyplot as plt
 from torchvision.utils import save_image
 from torch.utils.data import DataLoader
-from loader import RotDataset
+from loader import RotDataset, LandslideDataset
 from time import ctime
 from sacred import Experiment
+from model import FCNwBottleneck
 
 def validate(params, test_loader, _log):
     with th.no_grad():
@@ -13,7 +15,20 @@ def validate(params, test_loader, _log):
         shape = params['shape']
         res = th.zeros(shape)
         pad = params['pad']
-        model = th.load(params['load_model'])
+        # device = th.device("cuda:0" if th.cuda.is_available() else "cpu")
+        # trained_model = th.load(params['load_model'])
+        # trained_model.to(device)
+        # state = trained_model.state_dict()
+        # n_state = {}
+        # for s, v in state.items():
+        #     n_s = s[7:]
+        #     n_state[n_s] = v
+        if params['model'] == 'FCNwBottleneck':
+            model = FCNwBottleneck(params['num_feature'], params['pix_res']).cuda()
+        if th.cuda.device_count() > 1:
+            model = nn.DataParallel(model)
+        # import ipdb; ipdb.set_trace()
+        model.load_state_dict(th.load(params['load_model']))
         _log.info('[{}] model is successfully loaded.'.format(ctime()))
         test_iter = iter(test_loader)
         for iter_ in range(len(test_iter)):
@@ -24,7 +39,7 @@ def validate(params, test_loader, _log):
             prds[ignore] = 0
             del data, gt, ignore
             for idx in range(prds.shape[0]):
-                row, col = sample['index'][0][idx], sample['index'][1][idx]
+                row, col = int(sample['index'][0][idx].item()), int(sample['index'][1][idx].item())
                 res[row*params['ws']:(row+1)*params['ws'], col*params['ws']:(col+1)*params['ws']] = prds[idx, 0, :, :]
             _log.info('[{}]: writing [{}/{}]'.format(ctime(), iter_, len(test_iter)))
         _log.info('all images are written!')
@@ -36,7 +51,7 @@ ex = Experiment('validate_rotation')
 @ex.config
 def ex_cfg():
     params = {
-        'data_path': '/dev/shm/rotated_dataset.h5',
+        'data_path': '/home/ainaz/Projects/Landslides/image_data/rotated_landslide.h5',
         'load_model': '',
         'save_to': 'runs/result/',
         'region': 'Veneto',
@@ -45,12 +60,16 @@ def ex_cfg():
         'shape': (4201, 19250),
         'flag': 'test',
         'bs': 5,
-        'n_workers': 4
+        'n_workers': 4,
+        'num_feature': 94,
+        'pix_res': 10,
+        'model': 'FCNwBottleneck'
     }
 
 @ex.automain
 def main(params, _log):
     vd = RotDataset(params['data_path'], params['region'], 'test')
+    # vd = LandslideDataset(params['data_path'], params['region'], params['ws'], 'test', params['pad'])
     vd_loader = DataLoader(vd, batch_size=params['bs'], num_workers=params['n_workers'])
     _log.info('[{}] prepared the dataset for validation.'.format(ctime()))
     validate(params, vd_loader, _log)